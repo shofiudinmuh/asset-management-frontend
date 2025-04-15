eecdf226b4fcb4e7facb8d54e2eb6eb0eb23f5fe
shofiudinmuh <shofiudinmuh@gmail.com>
2025-04-15 08:07:49 +0700

    feat: auth pages

--- src/components/form/input/InputField.jsx
+++ src/components/form/input/InputField.jsx
@@ -1,4 +1,4 @@
-const Input = ({
+const InputField = ({
     type = 'text',
     id,
     name,
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+module.exports = {
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        extend: {},
+    },
+    plugins: [], // kosong atau isi plugin Tailwind lain jika perlu
 };
